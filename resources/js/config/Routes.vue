3f4ba102103a4106154acebc4044a03804b8ba33
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-05-19 18:29:20 +0100

    empty route to dashboard

--- resources/js/config/Routes.vue
+++ resources/js/config/Routes.vue
@@ -6,7 +6,8 @@ import Datatable from '../components/Datatable'
 
 const routes =[
     {path: '*', name: '404', component: Error404, meta: {}},
-    {path: '/', name: 'dashboard', component: Dashboard, meta: {}},
+    {path: '/', redirect:'/dashboard'},
+    {path: '/dashboard', name: 'dashboard', component: Dashboard, meta: {}},
     {path: '/shipments', name:'Shipments', component: Shipments, meta: {}},
     {path: '/datatable', name:'Datatable', component: Datatable, meta: {}}
 ];
