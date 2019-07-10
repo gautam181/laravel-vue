e8b060a7a1848ba35b818e2d58bcbe3c34625307
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-07-10 20:38:52 +0100

    ticket migration

---
+++ database/migrations/2019_06_28_164048_create_tickets_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('project_id')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('assigned_to')->nullable();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('assigned_to')->references('id')->on('users');
+            $table->index(['created_by', 'assigned_to', 'title']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
