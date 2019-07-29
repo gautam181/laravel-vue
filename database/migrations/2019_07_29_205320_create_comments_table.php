ca12e98584badf4ee24ce093ef5d82f2a13ebfd2
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-07-29 22:03:31 +0100

    comment migration created

---
+++ database/migrations/2019_07_29_205320_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('ticket_id');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('created_by');
+            $table->longText('comment')->nullable();
+            $table->timestamps();
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('created_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
