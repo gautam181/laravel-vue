59897a2b950cb35af4d5718e46a7deaef7a4a9b8
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-07-29 22:05:40 +0100

    comment api url added

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix'=>'/v1', 'middleware'=>['auth:api'] ], function (){
     Route::get('tickets', ['uses'=>'TicketController@index', 'as'=>'ticket.list']);
+    Route::get('ticket/{id}/comments', ['uses'=>'TicketController@index', 'as'=>'ticket.comments']);
     Route::resource('ticket', 'TicketController',['except'=>['index', 'create', 'edit']]);
+    Route::get('comments', ['uses'=>'CommentController@index', 'as'=>'comment.list']);
+    Route::resource('comment', 'CommentController',['except'=>['index', 'create', 'edit']]);
     Route::get('projects', ['uses'=>'ProjectController@index', 'as'=>'project.list']);
     Route::get('project/tickets/{id}', ['uses'=>'ProjectController@tickets', 'as'=>'project.ticket']);
     Route::resource('project', 'ProjectController',['except'=>['index', 'create', 'edit']]);
