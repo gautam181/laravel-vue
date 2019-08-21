11e46f24c62691855aee143102c8e60330ac927c
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-08-21 20:02:42 +0100

    login design fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,67 +2,73 @@
 
 @section('content')
 <div class="auth-container">
-    <div class="card">
-                <div class="auth-header">
-                    <div class="auth-title">{{ __('Login') }}</div>
+    <div class="login login-v1">
+        <!-- begin login-container -->
+        <div class="login-container">
+            <!-- begin login-header -->
+            <div class="login-header">
+                <div class="brand">
+                     <b>Authentication</b>
+                    <small>LaraVue App</small>
                 </div>
-
-                <div class="auth-content">
-                    <p class="text-center">LOGIN TO CONTINUE</p>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group">
-                            <label for="email" class="">{{ __('Username') }}</label>
-                                <input id="email" type="email" placeholder="your email" class="form-control underline @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password" class="">{{ __('Password') }}</label>
-
-                                <input id="password" type="password" placeholder="your password" class="form-control underline @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                <div class="icon">
+                    <i class="fa fa-lock"></i>
                 </div>
             </div>
+            <!-- end login-header -->
+            <!-- begin login-body -->
+            <div class="login-body">
+                <!-- begin login-content -->
+                <div class="login-content">
+                    <form action="{{ route('login') }}" method="POST" class="margin-bottom-0">
+                        @csrf
+                        <div class="form-group m-b-20">
+
+                            <input id="email" type="email" placeholder="your email" class="form-control underline form-control-sm  @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                        <div class="form-group m-b-20">
+                            <input id="password" type="password" placeholder="your password" class="form-control underline form-control-sm  @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                        <div class="checkbox checkbox-css m-b-20">
+                            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                            <label for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+
+                        <div class="login-buttons">
+                            <button type="submit" class="btn btn-success btn-block btn-md">Sign me in</button>
+                        </div>
+                    </form>
+                    @if (Route::has('password.request'))
+                        <div class="mt-20">
+                            <p>&nbsp;</p>
+                            Forgot password? Click
+                            <a class="" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                             to reset.
+                        </div>
+
+                    @endif
+                </div>
+                <!-- end login-content -->
+            </div>
+            <!-- end login-body -->
+        </div>
+        <!-- end login-container -->
+    </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,42 @@
     <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
 </head>
 <body>
-    <main class="auth">
+<div id="header" class="header navbar navbar-expand-md bg-dark  fixed-top">
+    <!-- begin container -->
+    <div class="container">
+        <!-- begin navbar-header -->
+        <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a href="index.html" class="navbar-brand">
+                <img src="/images/vue-laravel.png" alt="Laravel Vue App" class="brand-image">
+                </span>
+            </a>
+        </div>
+        <!-- end navbar-header -->
+        <!-- begin navbar-collapse -->
+        <div class="collapse navbar-collapse" id="header-navbar">
+
+            <ul class="nav navbar-nav ml-md-auto">
+                @if (Route::has('login'))
+                    @auth
+                        <li class="nav-item"><a href="/app" class="nav-link" data-click="scroll-to-target">Dashboard</a></li>
+                    @else
+                        <li class="nav-item"><a href="{{ route('login') }}" class="nav-link" data-click="scroll-to-target">LOGIN</a></li>
+
+                        @if (Route::has('register'))
+                            <li class="nav-item"><a href="{{ route('register') }}" class="nav-link" data-click="scroll-to-target">REGISTER</a></li>
+                        @endif
+                    @endauth
+                @endif
+            </ul>
+        </div>
+        <!-- end navbar-collapse -->
+    </div>
+    <!-- end container -->
+</div>
+    <main>
         @yield('content')
     </main>
 </body>
