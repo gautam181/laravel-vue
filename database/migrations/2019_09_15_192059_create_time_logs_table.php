e9dd8d05a22f3f2f9c9f84f1fb3c7f7b3485a087
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-09-15 20:43:11 +0100

    time log migration

---
+++ database/migrations/2019_09_15_192059_create_time_logs_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimeLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('time_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('ticket_id');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('user');
+            $table->date('when')->nullable(false);
+            $table->longText('description')->nullable();
+            $table->integer('hours')->nullable(false)->default(0);
+            $table->integer('minutes')->nullable(false)->default(0);
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+            $table->foreign('project_id')->references('id')->on('projects');
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('user')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('time_logs');
+    }
+}
