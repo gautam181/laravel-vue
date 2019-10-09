a211fa0340c5420d13b7d2f5a9b138cdaaff33da
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2019-10-09 21:30:16 +0100

    project loading icon bug fixed

--- resources/js/pages/projects/list/Active.vue
+++ resources/js/pages/projects/list/Active.vue
@@ -1,81 +1,77 @@
 <template>
     <div>
-        <div v-if="loaded">
-            <div class="row">
-                <div class="col-md-12">
-                    <filter-alert :total="pagination.totalRows" :show="!showFilters" id="project-list"></filter-alert>
-                </div>
-                <div class="col-md-12">
-                    <div class="list-options">
-                        <div>{{ pagination.totalRows}} results</div>
-                        <div class="btn-options text-right">
-                            <sort-filter :order="sortOrder" :selected="sortOption" v-on:update-sort="handleSort"  v-on:update-order="handleSortOrder" :options="sortOptions"></sort-filter>
-                        </div>
+        <div class="row" v-if="loaded">
+            <div class="col-md-12">
+                <filter-alert :total="pagination.totalRows" :show="!showFilters" id="project-list"></filter-alert>
+            </div>
+            <div class="col-md-12">
+                <div class="list-options">
+                    <div>{{ pagination.totalRows}} results</div>
+                    <div class="btn-options text-right">
+                        <sort-filter :order="sortOrder" :selected="sortOption" v-on:update-sort="handleSort"  v-on:update-order="handleSortOrder" :options="sortOptions"></sort-filter>
                     </div>
                 </div>
             </div>
-            <div class="row  list">
-                <loading-spinner label="Loading projects" :show="loading"></loading-spinner>
-                <template v-for="project in projects">
-                    <div :class="projectClasses +' projects'" v-if="showProject(project.progress)">
-                        <div class="panel green">
-                            <div class="panel-body">
-                                <div class="row">
-                                    <div class="col-sm-12">
-                                        <h5>
-                                            <router-link v-bind:to="{'name': 'project-tickets', params: {'id': project.id }}" active-class="" class="">
-                                                {{ project.name }}
-                                            </router-link>
-                                        </h5>
-                                        <div class="project-details">
-                                            {{ project.description}}
-                                        </div>
+        </div>
+        <div class="row  list" >
+            <loading-spinner label="Loading projects" :show="loading"></loading-spinner>
+            <template v-for="project in projects">
+                <div :class="projectClasses +' projects'" v-if="showProject(project.progress)">
+                    <div class="panel green">
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h5>
+                                        <router-link v-bind:to="{'name': 'project-tickets', params: {'id': project.id }}" active-class="" class="">
+                                            {{ project.name }}
+                                        </router-link>
+                                    </h5>
+                                    <div class="project-details">
+                                        {{ project.description}}
+                                    </div>
 
-                                        <div class="row project-actions">
-                                            <div class="col-sm-2 col">
-                                                <div class="project-label">OWNER</div>
-                                                <small v-if="project.owner" >
-                                                    <avatar :username="project.owner.name" :size="size" :customStyle="avatarStyle" v-b-tooltip.hover.rightbottom :title="project.owner.name"></avatar>
-                                                </small>
-                                            </div>
-                                            <div class="col-sm-4 col">
-                                                <div class="project-label" @click="editProject(project.id)">DEADLINE</div>
-                                                <small>{{ project.end_date | date }}</small>
-                                            </div>
-                                            <div class="col-sm-4 col">
-                                                <div class="project-label">PROGRESS</div>
-                                                <b-progress  :max="100" variant="success" class="m-t-xs full"  show-progress>
-                                                    <b-progress-bar :value="project.progress" :label="project.progress+'%'"></b-progress-bar>
-                                                </b-progress>
-                                            </div>
-                                            <div class="col-sm-2 col text-right">
-                                                <div class="project-label">&nbsp;</div>
-                                                <a href="javascript:void(0);" class="btn btn-sm btn-circle btn-default" :id="'project-'+project.id" >
-                                                    <i class="fa fa-ellipsis-v"></i>
-                                                </a>
-                                                <b-popover :target="'project-'+project.id" triggers="focus" placement="left">
-                                                    <template v-slot:title></template>
-                                                    <div class="popover-actions">
-                                                        <a href="javascript:void(0);" v-if="!completed" @click="deleteProject(project.id)"><i class="fa fa-times"></i>Delete</a>
-                                                        <a href="javascript:void(0);" @click="editProject(project.id)"><i class="fa fa-pen"></i>Edit</a>
-                                                        <a href="javascript:void(0);" v-if="!completed" @click="completeProject(project.id)"><i class="fa fa-check"></i> Complete</a>
-                                                    </div>
-                                                </b-popover>
-                                            </div>
+                                    <div class="row project-actions">
+                                        <div class="col-sm-2 col">
+                                            <div class="project-label">OWNER</div>
+                                            <small v-if="project.owner" >
+                                                <avatar :username="project.owner.name" :size="size" :customStyle="avatarStyle" v-b-tooltip.hover.rightbottom :title="project.owner.name"></avatar>
+                                            </small>
+                                        </div>
+                                        <div class="col-sm-4 col">
+                                            <div class="project-label" @click="editProject(project.id)">DEADLINE</div>
+                                            <small>{{ project.end_date | date }}</small>
+                                        </div>
+                                        <div class="col-sm-4 col">
+                                            <div class="project-label">PROGRESS</div>
+                                            <b-progress  :max="100" variant="success" class="m-t-xs full"  show-progress>
+                                                <b-progress-bar :value="project.progress" :label="project.progress+'%'"></b-progress-bar>
+                                            </b-progress>
+                                        </div>
+                                        <div class="col-sm-2 col text-right">
+                                            <div class="project-label">&nbsp;</div>
+                                            <a href="javascript:void(0);" class="btn btn-sm btn-circle btn-default" :id="'project-'+project.id" >
+                                                <i class="fa fa-ellipsis-v"></i>
+                                            </a>
+                                            <b-popover :target="'project-'+project.id" triggers="focus" placement="left">
+                                                <template v-slot:title></template>
+                                                <div class="popover-actions">
+                                                    <a href="javascript:void(0);" v-if="!completed" @click="deleteProject(project.id)"><i class="fa fa-times"></i>Delete</a>
+                                                    <a href="javascript:void(0);" @click="editProject(project.id)"><i class="fa fa-pen"></i>Edit</a>
+                                                    <a href="javascript:void(0);" v-if="!completed" @click="completeProject(project.id)"><i class="fa fa-check"></i> Complete</a>
+                                                </div>
+                                            </b-popover>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </template>
-                <div class="col-md-12">
-                    <load-more :pagination="pagination" v-on:load-more="loadMore"></load-more>
                 </div>
-
+            </template>
+            <div class="col-md-12" v-if="loaded">
+                <load-more :pagination="pagination" v-on:load-more="loadMore"></load-more>
             </div>
         </div>
-
     </div>
 </template>
 
@@ -95,7 +91,7 @@
                 project_id: 0,
                 loaded: false,
                 completed: this.$route.name == 'projects-active' ? 0:  1,
-                loading: false,
+                loading: true,
                 size:25,
                 popoverShow: false,
                 sortOptions: [
@@ -174,6 +170,7 @@
                 this.fetchProjects(true);
             },
             fetchProjects: function (val) {
+                this.loading = true;
                 let mode = val != undefined ? val : false;
                 if(!mode)
                     this.$root.$emit('project-list-loading', true);
