3a29eeaa741eff327d3d568cd6c92a460121b0e6
Abdul Wahhab Khan <abdulwahhabkhan@hotmail.com>
2020-05-30 01:09:14 +0100

    user credentials updated

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,7 +12,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class)->create(['email'=>'abdul@webequator.com', 'password' => 'Xplm@911']);
+        factory(User::class)->create(['email'=>'abdulwahhabkhan@webequator.com', 'password' => 'testlaravue']);
         factory(User::class, 10)->create();
     }
 }
